482c550632972178eb660849ad451914b1a2a67b
AryanPawr <86268950+AryanPawr@users.noreply.github.com>
2021-06-26 17:03:05 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Heading from "./Heading"
+import Footer from "./Footer"
+import Info from"./Info"
+function App(){
+  return (
+  <div>
+    <Heading />
+    <Footer />
+    <Info />
+    <Info />
+    <Info />
+    <Info />
+  </div>
+  );
+}
+export default App;
---
+++ Info.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+function Info() {
+  return (
+    <div className="note">
+    
+      <h1>Javascript and React.js</h1>
+      <p>A bootcamp Held for 7 Days by Shaurya Sinha</p>
+      </div>
+  );
+}
+export default Info ;
